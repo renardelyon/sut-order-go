c07d77aa2011f0655b3241fcac3f486de96c8163
renardelyon <renard.elyon.r@gmail.com>
2022-06-28 17:19:15 +0700

    Create scraping helper

---
+++ lib/utils/scrapping.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func Scraping(url string) (*goquery.Document, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
+
+}
