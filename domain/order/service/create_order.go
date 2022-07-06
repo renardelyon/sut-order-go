b495d21ab74c0a67c96cad66883b75b48d392afa
renardelyon <renard.elyon.r@gmail.com>
2022-07-06 15:11:11 +0700

    Create create order handler

---
+++ domain/order/service/create_order.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"context"
+	"net/http"
+	omodel "sut-order-go/domain/order/model"
+	pmodel "sut-order-go/domain/product/model"
+	pb "sut-order-go/pb/order"
+)
+
+func (s *Service) CreateOrder(ctx context.Context, reqCreated *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
+	if result := s.H.DB.Where(&omodel.Order{Id: reqCreated.UserId, ProductName: reqCreated.ProductName}); result.Error == nil {
+		return &pb.CreateOrderResponse{
+			Status: http.StatusBadRequest,
+			Error:  "Product have been ordered previously",
+		}, nil
+	}
+
+	s.H.DB.Create(&omodel.Order{
+		Id:          reqCreated.UserId,
+		ProductName: reqCreated.ProductName,
+	})
+
+	if res, _ := s.productInterface.SaveRequestedGift(pmodel.ProductInfo{
+		AdminId:     reqCreated.AdminId,
+		Fullname:    reqCreated.Fullname,
+		Username:    reqCreated.Fullname,
+		UserId:      reqCreated.UserId,
+		Productname: reqCreated.ProductName,
+		Url:         reqCreated.Url,
+	}); res.Error != "" {
+		return &pb.CreateOrderResponse{
+			Status: res.Status,
+			Error:  res.Error,
+		}, nil
+	}
+
+	return &pb.CreateOrderResponse{
+		Status: http.StatusOK,
+	}, nil
+}
